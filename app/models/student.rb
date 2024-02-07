13bc77f41c1eab268dc7a2ee86dc8175f4e8229e
Zeeshan Gulzar <xeeshangulzar@gmail.com>
2024-02-07 17:06:34 +0500

    Has and belongs to many

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,4 @@
 class Student < ApplicationRecord
   has_many :blogs, dependent: :destroy
+  has_and_belongs_to_many :courses
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_07_112828) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_07_114605) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -24,6 +24,11 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_07_112828) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "courses_students", id: false, force: :cascade do |t|
+    t.integer "course_id", null: false
+    t.integer "student_id", null: false
   end
 
   create_table "students", force: :cascade do |t|
