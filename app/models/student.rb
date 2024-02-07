e07f583af4315446d1cfd1d09abcc94db685fe48
Zeeshan Gulzar <xeeshangulzar@gmail.com>
2024-02-07 17:09:53 +0500

    Manualy writing join table name

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,4 @@
 class Student < ApplicationRecord
   has_many :blogs, dependent: :destroy
-  has_and_belongs_to_many :courses
+  has_and_belongs_to_many :courses, join_table: 'students_courses'
 end
