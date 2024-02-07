4347bd2d89aa77718bcd1d23367e31b4761f5320
Zeeshan Gulzar <xeeshangulzar@gmail.com>
2024-02-07 17:31:52 +0500

    has_many_through

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,6 @@
 class Student < ApplicationRecord
   has_many :blogs, dependent: :destroy
-  has_and_belongs_to_many :courses, join_table: 'students_courses'
+  has_and_belongs_to_many :courses
+  has_many :student_projects
+  has_many :projects, through: :student_projects
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_07_114605) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_07_122034) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -31,6 +31,23 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_07_114605) do
     t.integer "student_id", null: false
   end
 
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "student_projects", force: :cascade do |t|
+    t.integer "student_id", null: false
+    t.integer "project_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.date "submission"
+    t.index ["project_id"], name: "index_student_projects_on_project_id"
+    t.index ["student_id"], name: "index_student_projects_on_student_id"
+  end
+
   create_table "students", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -39,4 +56,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_07_114605) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "student_projects", "projects"
+  add_foreign_key "student_projects", "students"
 end
