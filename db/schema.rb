1594c3d3fc1ec6d4c286a9a357a89c3d5ad7df17
Zeeshan Gulzar <xeeshangulzar@gmail.com>
2024-02-07 18:30:56 +0500

    Has One

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_07_122034) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_07_131528) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -29,6 +29,37 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_07_122034) do
   create_table "courses_students", id: false, force: :cascade do |t|
     t.integer "course_id", null: false
     t.integer "student_id", null: false
+  end
+
+  create_table "demos", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "departments", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "manager_histories", force: :cascade do |t|
+    t.date "joining_date"
+    t.string "total_experience"
+    t.integer "manager_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["manager_id"], name: "index_manager_histories_on_manager_id"
+  end
+
+  create_table "managers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.integer "contact"
+    t.integer "department_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["department_id"], name: "index_managers_on_department_id"
   end
 
   create_table "projects", force: :cascade do |t|
@@ -56,6 +87,17 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_07_122034) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "sub_demos", force: :cascade do |t|
+    t.string "title"
+    t.integer "demo_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["demo_id"], name: "index_sub_demos_on_demo_id"
+  end
+
+  add_foreign_key "manager_histories", "managers"
+  add_foreign_key "managers", "departments"
   add_foreign_key "student_projects", "projects"
   add_foreign_key "student_projects", "students"
+  add_foreign_key "sub_demos", "demos"
 end
